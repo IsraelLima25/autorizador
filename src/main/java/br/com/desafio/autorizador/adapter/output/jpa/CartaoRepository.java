a1bce67f96d1ec8b72c802eb17addfc5810d92b6
Israel Santos Lima <israellima@dcc.ufmg.br>
2025-09-14 16:55:07 -0300

    refactor: melhoria performance

--- src/main/java/br/com/desafio/autorizador/adapter/output/jpa/CartaoRepository.java
+++ src/main/java/br/com/desafio/autorizador/adapter/output/jpa/CartaoRepository.java
@@ -1,10 +1,8 @@
 package br.com.desafio.autorizador.adapter.output.jpa;
 
 import jakarta.persistence.LockModeType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import jakarta.persistence.QueryHint;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
@@ -22,6 +20,7 @@ public interface CartaoRepository extends JpaRepository<CartaoEntity, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select c from CartaoEntity c where c.numero = :numero")
+    @QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000"))
     Optional<CartaoEntity> obterPorNumeroParaAtualizar(@Param("numero") String numero);
 
     @Modifying(flushAutomatically = true, clearAutomatically = false)
